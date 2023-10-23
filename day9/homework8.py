b08718acde24da5118504cc116ddf44c121dd0be
temo697 <145621806+temo697@users.noreply.github.com>
2023-10-23 13:07:41 +0400

    Add files via upload

---
+++ day9/homework8.py
@@ -0,0 +1,15 @@
+group_4 =["Giorgi Chkhetiani ","giorgi lobzhanidze","Guri Jishkariani ","beqa giorgobiani","Merab Tsitskhvaia"]
+a_counter = 0
+
+for element in group_4:
+    for char in element:
+        if char ==  "a":
+            a_counter += 1
+print(a_counter)
+
+i_counter = 0
+for element in group_4:
+    for char in element:
+        if char ==  "i":
+            i_counter += 1
+print(i_counter)
---
+++ day9/homework9.py
@@ -0,0 +1,4 @@
+my_list = [25,29,50,98]
+my_list[1] = 69
+print(my_list)
+print(25+69+50+98)
