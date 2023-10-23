e62ce29dbbb9769e91f6520e9456f8f2e4e8fad8
temo697 <145621806+temo697@users.noreply.github.com>
2023-10-23 13:03:57 +0400

    Add files via upload

---
+++ day9/homework10.py
@@ -0,0 +1,4 @@
+exam = [42-20,20+43,55+2,27-20,]
+print(((str(exam[0]) + str(" is true"))+ str(exam[1]) + " "  + str(" is true") +  " " + str(exam[2]) + str(" is true")) +" " + (str(exam[3]) + " " +  str(" is true")))
+if print >0:
+    print("შენ აშკარად GOA ში სწავლობდი ჩემო ძმაო" )
