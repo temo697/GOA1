e6772fe410dd3fe7ec07e37a6f467c7923960661
temo697 <145621806+temo697@users.noreply.github.com>
2023-10-23 13:04:57 +0400

    Update homework10.py

--- day9/homework10.py
+++ day9/homework10.py
@@ -1,4 +1,2 @@
-exam = [42-20,20+43,55+2,27-20,]
-print(((str(exam[0]) + str(" is true"))+ str(exam[1]) + " "  + str(" is true") +  " " + str(exam[2]) + str(" is true")) +" " + (str(exam[3]) + " " +  str(" is true")))
-if print >0:
-    print("შენ აშკარად GOA ში სწავლობდი ჩემო ძმაო" )
+user = [42-20,20+43,55+2,27-20,]
+print(((str(user[0]) + str(" is true"))+ str(user[1]) + " "  + str(" is true") +  " " + str(user[2]) + str(" is true")) +" " + (str(user[3]) + " " +  str(" is true")))
