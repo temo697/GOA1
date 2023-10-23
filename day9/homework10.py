8edb7c699065c6a98ac843e87e66f9babeb25e22
temo697 <145621806+temo697@users.noreply.github.com>
2023-10-23 13:06:34 +0400

    Delete day9/homework10.py

--- day9/homework10.py
+++
@@ -1,2 +0,0 @@
-user = [42-20,20+43,55+2,27-20,]
-print(((str(user[0]) + str(" is true"))+ str(user[1]) + " "  + str(" is true") +  " " + str(user[2]) + str(" is true")) +" " + (str(user[3]) + " " +  str(" is true")))
