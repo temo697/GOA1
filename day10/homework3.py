e3c8bcf0959e4f11b97a0be9572832c9038c5a0e
temo697 <145621806+temo697@users.noreply.github.com>
2023-10-28 18:36:08 +0400

    Update homework3.py

--- day10/homework3.py
+++ day10/homework3.py
@@ -1,5 +1,23 @@
 
-import random
+import random 
+my_arr = []
+chars = list("qwertyuiopasdfghjklzxcvbnm")
+symbols = list("!@#$%^&*")
+numbers = list("1234567890")
+for i in range(9):
+    random_num = random.choice(numbers)
+    my_arr.append(random_num)
+    numbers.remove(random_num)
 
-num = random.randint(0,100)
-print(num)
+for i in range(25):
+    random_char = random.choice(chars)
+    my_arr.append(random_char)
+    chars.remove(random_char)
+
+for i in range(6):
+    random_symbol = random.choice(symbols)
+    my_arr.append(random_symbol)
+    symbols.remove(random_symbol)
+
+
+print("the program chose these characters:" , my_arr))
